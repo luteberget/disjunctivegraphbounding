27c0fabcc1d53ba495e383257ddf1218022c6fe5
Bjørnar Luteberget <luteberget@gmail.com>
2024-11-24 21:30:17 +0100

    randomrail

--- randomrail/main.js
+++ randomrail/main.js
@@ -1,89 +1,98 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-fetch("l1.json")
-  .then(response => response.json())
-  .then(main);
-
-function main(data) {
-    
-}
-
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// const scene = new THREE.Scene();
-
-// const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
-
-// const controls = new OrbitControls( camera, renderer.domElement );
-
-// //controls.update() must be called after any manual changes to the camera's transform
-// camera.position.set( 0, 20, 100 );
-// controls.update();
-
-// function animate() {
-
-// 	requestAnimationFrame( animate );
-
-// 	// required if controls.enableDamping or controls.autoRotate are set to true
-// 	controls.update();
-
-// 	renderer.render( scene, camera );
-
-// }
-
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xbbbbbb );
+const material1 = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 3 } );
+const material2 = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 1 } );
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+fetch("i1.json").then(r => r.json()).then(add_infrastructure);
+function add_infrastructure(infrastructure) {
+
+  const points = [];
+  for(const node of infrastructure.nodes) {
+    points.push(node.location.x, node.location.y, 0.0);
+  }
+
+  const index = [];
+  for(const res of infrastructure.resources) {
+
+    index.push(res.node_a);
+    for(const pt of res.line_segments) {
+      index.push(points.length/3);
+      points.push(pt.x,pt.y,0.0);
+    }
+    index.push(res.node_b);
+    // 65535 is the restart index for Uint16
+    index.push(65535);
+  }
+
+  console.log("point", points);
+  console.log("index", index);
+
+  const infrastructureGeometry = new THREE.BufferGeometry();
+  const positionAttribute = new THREE.Float32BufferAttribute( points, 3 );
+  const indexAttribute = new THREE.Uint16BufferAttribute(index, 1 ); 
+  infrastructureGeometry.setAttribute( 'position', positionAttribute );
+  infrastructureGeometry.setIndex( indexAttribute );
+  
+  const line = new THREE.Line( infrastructureGeometry, material1 );
+  scene.add( line );
+
+
+
+  fetch("tt1.json").then(r => r.json()).then(add_timetable);
+  function add_timetable(timetable) {
+
+    console.log("TT");
+    const time_scale = 1.0 / 3600.0;
+    const points = [];
+    const index = [];
+    for(const train of timetable.trains) {
+      for(const [op_idx,op] of train.operations.entries()) {
+        const res = infrastructure.resources[op.resource];
+        const node_a = infrastructure.nodes[res.node_a];
+        const node_b = infrastructure.nodes[res.node_b];
+        const pt = op.forward ? node_a.location : node_b.location;
+        index.push(points.length/3);
+        points.push(pt.x, pt.y, op.time *time_scale);
+        if (op_idx + 1 == train.operations.length) {
+          const pt = (!op.forward) ? node_a.location : node_b.location;
+          index.push(points.length/3);
+          points.push(pt.x, pt.y, (op.time + op.min_duration) *time_scale);
+        }
+      }
+      index.push(65535);
+    }
+    console.log(points);
+    console.log(index);
+
+    const geometry = new THREE.BufferGeometry();
+    const positionAttribute = new THREE.Float32BufferAttribute( points, 3 );
+    const indexAttribute = new THREE.Uint16BufferAttribute(index, 1 ); 
+    geometry.setAttribute( 'position', positionAttribute );
+    geometry.setIndex( indexAttribute );
+    
+    const line = new THREE.Line( geometry, material2 );
+    scene.add( line );
+
+    for(let i = 0; i < 5; i += 1) {
+      const l = new THREE.Line( infrastructureGeometry, material1 );
+      l.translateZ(i);
+      scene.add( l );
+    }
+  }
+}
+
+
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-
-//create a blue LineBasicMaterial
-const blue = new THREE.LineBasicMaterial( { color: 0xff0000, linewidth: 5 } );
-
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-
-const geometry = new THREE.BufferGeometry();
-const positionAttribute = new THREE.Float32BufferAttribute( [
-  - 1,   1, 0,
-  - 1, - 1, 0, 
-    0,   1, 0,
-    0, - 1, 0,
-    1,   1, 0,
-    1, - 1, 0
-], 3 );
-
-// 65535 is the restart index for Uint16
-const index = new THREE.Uint16BufferAttribute( [ 0, 1, 2, 65535, 3, 4, 5 ], 1 ); 
-
-geometry.setAttribute( 'position', positionAttribute );
-geometry.setIndex( index );
-
-// matLine = new THREE.Line2NodeMaterial( {
-
-//     color: 0xffffff,
-//     linewidth: 5, // in world units with size attenuation, pixels otherwise
-//     vertexColors: true,
-//     dashed: false,
-//     alphaToCoverage: true,
-
-// } );
-
-const line = new THREE.Line( geometry, blue );
-scene.add( line );
-
-
-camera.position.z = 5;
+camera.position.z = 500;
 camera.position.x = 5;
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
@@ -91,8 +100,5 @@ controls.update();
 function animate() {
 	renderer.render( scene, camera );
     controls.update();
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
--- randomrail/src/main.rs
+++ randomrail/src/main.rs
@@ -1,4 +1,4 @@
-use std::f64::consts::TAU;
+use std::{collections::HashMap, f64::consts::TAU};
 
 use rand::Rng;
 use rand_distr::{Distribution, Normal};
@@ -17,6 +17,7 @@ pub struct Node {
 
 #[derive(Serialize)]
 pub struct Resource {
+    length: f64,
     node_a: usize,
     node_b: usize,
     line_segments: Vec<Vec2>,
@@ -28,6 +29,24 @@ pub struct Infrastructure {
     resources: Vec<Resource>,
 }
 
+#[derive(Serialize)]
+pub struct Timetable {
+    trains: Vec<Train>,
+}
+
+#[derive(Serialize)]
+pub struct Train {
+    operations: Vec<Operation>,
+}
+
+#[derive(Serialize)]
+pub struct Operation {
+    resource: usize,
+    forward: bool,
+    min_duration: f64,
+    time: f64,
+}
+
 pub fn add_simple_line(
     infrastructure: &mut Infrastructure,
     mut track_start_node: usize,
@@ -37,26 +56,23 @@ pub fn add_simple_line(
     let min_stations = 10;
     let max_stations = 30;
 
-    let station_tracks = [1, 2, 2, 2, 2, 3, 4, 5];
+    let station_tracks = [1, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5];
 
     let avg_dist = 10.0f64;
     let min_dist = 1.0;
     let dist_stddev = 8.0;
     let dist = Normal::new(avg_dist, dist_stddev).unwrap();
 
-    let station_length = 0.2;
-
     let num_stations = rng.gen_range(min_stations..=max_stations);
+    println!("Line: generating {} stations", num_stations);
+    let mut total_length = 0.0;
     for _ in 0..num_stations {
         let track_length = dist.sample(&mut rng).max(min_dist);
+        total_length += track_length;
         let start_loc = infrastructure.nodes[track_start_node].location;
         let track_end_loc = Vec2 {
-            x: start_loc.x + track_length * direction.x,
-            y: start_loc.y + track_length * direction.y,
-        };
-        let station_end_loc = Vec2 {
-            x: track_end_loc.x + station_length * direction.x,
-            y: track_end_loc.y + station_length * direction.y,
+            x: start_loc.x + direction.x,
+            y: start_loc.y + direction.y,
         };
 
         let track_end_node = infrastructure.nodes.len();
@@ -68,40 +84,65 @@ pub fn add_simple_line(
             node_a: track_start_node,
             node_b: track_end_node,
             line_segments: vec![],
-        });
-
-        let station_end_node = infrastructure.nodes.len();
-        infrastructure.nodes.push(Node {
-            location: station_end_loc,
+            length: track_length,
         });
 
         let n_station_tracks = station_tracks[rng.gen_range(0..station_tracks.len())];
-        for track in 0..n_station_tracks {
-            let corner_dist = track as f64 * 0.025;
 
-            let corner1 = Vec2 {
-                x: track_end_loc.x + corner_dist * direction.x - corner_dist * direction.y,
-                y: track_end_loc.y + corner_dist * direction.y + corner_dist * direction.x,
-            };
-
-            let c2c = station_length - 2.0 * corner_dist;
-
-            let corner2 = Vec2 {
-                x: corner1.x + c2c * direction.x,
-                y: corner1.y + c2c * direction.x,
-            };
-
-            infrastructure.resources.push(Resource {
-                node_a: track_end_node,
-                node_b: station_end_node,
-                line_segments: vec![corner1, corner2],
-            });
-        }
+        let station_end_node =
+            add_station(infrastructure, n_station_tracks, track_end_node, direction);
 
         track_start_node = station_end_node;
     }
 
+    println!("  total length {total_length:.2}");
+
     track_start_node
+}
+
+fn add_station(
+    infrastructure: &mut Infrastructure,
+    n_station_tracks: i32,
+    start_node: usize,
+    direction: Vec2,
+) -> usize {
+    const CORNER_DIST_PER_TRACK: f64 = 0.025;
+    let station_viz_length = CORNER_DIST_PER_TRACK * 2.0 * (n_station_tracks as f64 + 2.0);
+
+    let track_end_loc = infrastructure.nodes[start_node].location;
+    let station_end_loc = Vec2 {
+        x: track_end_loc.x + station_viz_length * direction.x,
+        y: track_end_loc.y + station_viz_length * direction.y,
+    };
+
+    let station_end_node = infrastructure.nodes.len();
+    infrastructure.nodes.push(Node {
+        location: station_end_loc,
+    });
+
+    for track in 0..n_station_tracks {
+        let corner_dist = track as f64 * CORNER_DIST_PER_TRACK;
+
+        let corner1 = Vec2 {
+            x: track_end_loc.x + corner_dist * direction.x - corner_dist * direction.y,
+            y: track_end_loc.y + corner_dist * direction.y + corner_dist * direction.x,
+        };
+
+        let c2c = station_viz_length - 2.0 * corner_dist;
+
+        let corner2 = Vec2 {
+            x: corner1.x + c2c * direction.x,
+            y: corner1.y + c2c * direction.y,
+        };
+
+        infrastructure.resources.push(Resource {
+            node_a: start_node,
+            node_b: station_end_node,
+            line_segments: vec![corner1, corner2],
+            length: 0.2,
+        });
+    }
+    station_end_node
 }
 
 fn main() {
@@ -113,20 +154,116 @@ fn main() {
         location: Vec2 { x: 0., y: 0. },
     });
 
-    let n_lines = 1;
-    let mut lines: Vec<(usize, usize)> = Vec::new();
-    let center_node = 0;
+    let n_lines = 3;
+    let mut lines: Vec<Vec<usize>> = Vec::new();
 
-    for i in 0..n_lines {
-        let angle: f64 = (i as f64 / n_lines as f64) * TAU;
-        let direction = Vec2 {
-            x: angle.cos(),
-            y: -angle.sin(),
-        };
+    let main_direction = Vec2 { x: 1.0, y: 0.0 };
+    let main_station_west = 0;
+    let main_station_east = add_station(&mut infrastructure, 20, main_station_west, main_direction);
 
-        let line_end_node = add_simple_line(&mut infrastructure, 0, direction);
-        lines.push((center_node, line_end_node));
+    for (const_angle, start_node) in [(0.0, main_station_east), (0.5, main_station_west)] {
+        for i in 0..n_lines {
+            let range = 1. / 7.;
+            let angle =
+                (const_angle + -range + 2. * range * (i as f64 / (n_lines - 1).max(1) as f64))
+                    * TAU;
+            println!("angle1 {}", angle);
+            let direction = Vec2 {
+                x: angle.cos(),
+                y: -angle.sin(),
+            };
+            let start_idx = infrastructure.nodes.len();
+            let line_end_node = add_simple_line(&mut infrastructure, start_node, direction);
+            let mut line = Vec::new();
+            line.push(start_node);
+            line.extend(start_idx..=line_end_node);
+            lines.push(line);
+        }
     }
 
-    std::fs::write("l1.json", serde_json::to_string(&infrastructure).unwrap()).unwrap();
+    std::fs::write("i1.json", serde_json::to_string(&infrastructure).unwrap()).unwrap();
+
+    // TIMETABLE
+    let mut timetable = Timetable { trains: Vec::new() };
+    for line in &lines {
+        const SPEED :f64 = 60.0 /* km/h */ / 3600.0 /* km/sec */;
+        const SPAN: f64 = 6. * 3600.; // 6 hours
+        const SLACK: f64 = 1.04; // 4% running time slack
+        let freq = rand::thread_rng().gen_range(1.0..=4.0);
+
+        println!("line freq {freq}");
+
+        let line_trains = generate_trains(&infrastructure, line, SPEED, SPAN, SLACK, freq);
+        timetable.trains.extend(line_trains.into_iter());
+    }
+
+    std::fs::write("tt1.json", serde_json::to_string(&timetable).unwrap()).unwrap();
+}
+
+pub fn generate_trains(
+    infrastructure: &Infrastructure,
+    line: &[usize],
+    speed: f64,
+    span: f64,
+    slack: f64,
+    normalized_frequency: f64,
+) -> Vec<Train> {
+    let mut trains = Vec::new();
+
+    let mut up_resources: HashMap<usize, Vec<usize>> = Default::default();
+    let mut down_resources: HashMap<usize, Vec<usize>> = Default::default();
+
+    for (res_idx, res) in infrastructure.resources.iter().enumerate() {
+        up_resources.entry(res.node_a).or_default().push(res_idx);
+        down_resources.entry(res.node_b).or_default().push(res_idx);
+    }
+
+    let oneway_actual_minimum_running_time = line
+        .iter()
+        .enumerate()
+        .filter_map(|(line_idx, node_idx)| {
+            let ress = up_resources.get(node_idx);
+            // Only the last node should not have an up-going resource.
+            assert!(ress.is_some() || line_idx + 1 == line.len());
+            ress.map(|ress| infrastructure.resources[ress[0]].length / speed)
+        })
+        .sum::<f64>();
+
+    let oneway = oneway_actual_minimum_running_time * (1.0 + slack);
+
+    let period = 2. * oneway / normalized_frequency;
+    // when does the same train start again?
+    let next_period = (2. * oneway / period).ceil() * period;
+    for (start, up) in [(0.0, true), (0.5 * next_period, false)] {
+        for i in 0..=((span / period).round() as i32) {
+            let mut time = start + i as f64 * period;
+            let line: Box<dyn Iterator<Item = _>> = if up {
+                Box::new(line.iter())
+            } else {
+                Box::new(line.iter().rev())
+            };
+            let operations = line
+                .filter_map(|n| {
+                    if up { &up_resources } else { &down_resources }
+                        .get(n)
+                        .map(|x| x[0])
+                })
+                .map(|x| {
+                    let min_duration = infrastructure.resources[x].length / speed;
+                    let op = Operation {
+                        forward: up,
+                        resource: x,
+                        min_duration,
+                        time,
+                    };
+                    let dt = min_duration * (1.0 + slack);
+                    time += dt;
+                    op
+                })
+                .collect();
+            trains.push(Train { operations });
+        }
+    }
+
+    trains
 }
